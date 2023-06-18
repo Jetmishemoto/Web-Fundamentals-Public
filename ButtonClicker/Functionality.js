d716bf20bd5bef6f98ca3af6cc2b8fbdd210d6cb
Jetmishemoto <Daadoniwp92@gmx.com>
2023-06-18 18:13:26 -0400

    added a few functions

---
+++ ButtonClicker/Functionality.js
@@ -0,0 +1,22 @@
+
+
+
+function LogOut(Element){
+    
+    if(Element.innerText == "Login"){
+     Element.innerText = "logout";
+
+    }else Element.innerText = "Login";
+
+}
+
+
+
+
+
+function RemoveDefBnt(Element){
+    
+    Element.remove();
+}
+
+
