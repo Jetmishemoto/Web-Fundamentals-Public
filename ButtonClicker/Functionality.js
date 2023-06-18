b12a184315d88de6fd959fddf89a0fde033c5d42
Jetmishemoto <Daadoniwp92@gmx.com>
2023-06-18 19:20:36 -0400

    ..

--- ButtonClicker/Functionality.js
+++ ButtonClicker/Functionality.js
@@ -6,19 +6,17 @@ function LogOut(Element){
     if(Element.innerText == "Login"){
      Element.innerText = "logout";
     }else Element.innerText = "Login";
-
 }
 
 
 
-function RemoveDefBnt(Element){
-    Element.remove();
-}
+function RemoveDefBnt(Element) 
+Element.remove();
 
 
 
-function likealert(){
-    alert("Ninja was liked! Sweet!");
+function likealert()
+alert("Ninja was liked! Sweet!");
 
-}
+
 
