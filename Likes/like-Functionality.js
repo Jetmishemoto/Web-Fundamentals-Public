4e675ba731d5df9fd329296bbc12ab95d070dc35
Jetmishemoto <Daadoniwp92@gmx.com>
2023-06-19 16:47:57 -0400

    finishing touches

--- Likes/like-Functionality.js
+++ Likes/like-Functionality.js
@@ -1,13 +1,34 @@
 
 var ilikecount = document.querySelector("#likenum");
+var ilikecount1 = document.querySelector("#likenum1");
+var ilikecount2 = document.querySelector("#likenum2");
 var count = 0;
+var count1 = 0;
+var count2 = 0;
 
 
-console.log(ilikecount);
+// console.log(ilikecount);
 
-function likebtnIncrement(){
-    count++;
-    ilikecount.innerText = count;
-    console.log(count)
+function likebtnIncrement(id){
+    console.log(id)
+    
+    if(id == 1){
+        count++;
+
+        ilikecount.innerText = count;
+       
+    }else if(id == 2){
+
+        count1++;
+        ilikecount1.innerText = count1;
+        
+
+    } else if(id == 3){
+        count2++;
+
+        ilikecount2.innerText = count2;
+    }
+    
+    
 }
 
